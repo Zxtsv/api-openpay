d8e43a1ddd2e34e057e6c024c719675359119ea0
Bernardo <bernardo_pprz@comunidad.unam.mx>
2024-05-27 01:50:51 -0600

    OPENPAY-API | REALESE: Add .jar and Application

---
+++ src/main/java/mx/openpay/api_openpay/ApiOpenpayApplication.java
@@ -0,0 +1,13 @@
+package mx.openpay.api_openpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOpenpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOpenpayApplication.class, args);
+	}
+
+}
